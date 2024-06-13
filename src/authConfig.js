206c340954b160e9b59fad59c4ad15f41c31ab04
sandeepkumar <sandeepkumar.bala@aspiresys.com>
2024-06-13 20:33:00 +0530

    Redirect uri

--- src/authConfig.js
+++ src/authConfig.js
@@ -6,7 +6,7 @@ export const msalConfig = {
     auth: {
         clientId: "6375753e-3b1b-4eac-a731-501b08b3bbf4",
         authority: "https://login.microsoftonline.com/42f65d85-0044-4e88-9d17-5aeff505072a",
-        redirectUri: "https://lively-mushroom-0857d2900.5.azurestaticapps.net/auth-end", // or your production URL
+        redirectUri: "/redirect", // or your production URL
     },
     cache: {
         cacheLocation: "localStorage",
